dc9f976e400f50f7a4835124192418163d588fc6
gayashan4lk <gayashan4lk@gmail.com>
2026-03-14 14:02:55 +0530

    update text

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 			<main>
 				<h1 className="text-5xl font-black">Welcome</h1>
 				<div className="@container">
-					<div className="mx-auto @sm:w-sm">Create Daily Expenses Form</div>
+					<div className="mx-auto @sm:w-sm">Enter Daily Expenses</div>
 				</div>
 			</main>
 		</div>
